f5924759a4ec826216d3498b6d625c09b8cc6ef0
Ty <turret1226@gmail.com>
2020-12-31 11:27:50 -0700

    Added a very basic test script.

---
+++ test.py
@@ -0,0 +1,11 @@
+# txt2img
+# Make images out of basic text.
+#
+# Copyright (C) 2020, Ty Gillespie. ALl rights reserved.
+# MIT License.
+
+import txt2img
+
+
+if __name__ == "__main__":
+    txt2img.generate(200, 200, "This is an example image", "white", 10, 10, "black", "your_image.png")
