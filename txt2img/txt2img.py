3dc0075b0e419df5ef5b873798e75f45cb1e1ae3
Ty <turret1226@gmail.com>
2021-01-12 12:57:01 -0700

    Typos and some more formatting changes.

--- txt2img/txt2img.py
+++ txt2img/txt2img.py
@@ -8,27 +8,27 @@ from PIL import Image, ImageDraw
 
 
 def generate(
-    width: int,
-    height: int,
-    text: str,
-    bg: str = "white",
-    text_x: int = 10,
-    text_y: int = 10,
-    fg: str = "black",
-    filename: str = "image.png",
+ width: int,
+ height: int,
+ text: str,
+ bg: str = "white",
+ text_x: int = 10,
+ text_y: int = 10,
+ fg: str = "black",
+ filename: str = "image.png",
 ):
-    """Generate an image.
-    Parems:
-        width: the width (in pixels), of the image.
-        height: the height (in pixels) of the image.
-        text: the text that goes on the image.
-        bg (optional): the background color (by default white).
-        text_x (optional): the top x of the text. By default 10.
-        text_y (optional): the top y of the text. By default 10.
-        fg (optional): the foreground color. By default black.
-        filename (optional): the output filename (by default image.png).
-    """
-    i = Image.new("RGB", (width, height), color=bg)
-    d = ImageDraw.Draw(i)
-    d.text((text_x, text_y), text, fill=fg)
-    i.save(filename)
+ """Generate an image.
+ Parems:
+  width: the width (in pixels), of the image.
+  height: the height (in pixels) of the image.
+  text: the text that goes on the image.
+  bg (optional): the background color (by default white).
+  text_x (optional): the top x of the text. By default 10.
+  text_y (optional): the top y of the text. By default 10.
+  fg (optional): the foreground color. By default black.
+  filename (optional): the output filename (by default image.png).
+ """
+ i = Image.new("RGB", (width, height), color=bg)
+ d = ImageDraw.Draw(i)
+ d.text((text_x, text_y), text, fill=fg)
+ i.save(filename)
--- txt2img/txt2img_cli.py
+++ txt2img/txt2img_cli.py
@@ -1,7 +1,7 @@
 # txt2img
 # Make images out of basic text.
 #
-# Copyright (C) 2020, Ty Gillespie. ALl rights reserved.
+# Copyright (C) 2020-2021, Ty Gillespie. All rights reserved.
 # MIT License.
 
 import txt2img
@@ -19,6 +19,6 @@ parser.add_argument('--fg', default='black', help='Text foreground colour')
 parser.add_argument('-f', '--filename', type=argparse.FileType('wb'), default='image.png', help='Where to save the image')
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    txt2img.generate(args.width, args.height, args.text, args.bg, args.text_x, args.text_y, args.fg, args.filename)
-    print("Done!")
+ args = parser.parse_args()
+ txt2img.generate(args.width, args.height, args.text, args.bg, args.text_x, args.text_y, args.fg, args.filename)
+ print("Done!")
